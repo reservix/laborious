63b662204a786e7f4d643a819a5374c157b22a6c
Sebastian Sebald <sebastian.sebald@gmail.com>
2019-02-13 11:03:53 +0100

    🐛 Catch when token file can not be found (#10)

--- src/gitlab/token.test.ts
+++ src/gitlab/token.test.ts
@@ -1,14 +1,12 @@
+import { readFile, outputFile } from 'fs-extra';
 import opn from 'opn';
 import tempy from 'tempy';
 
-import { createGitlabToken } from './token';
-import { outputFile } from 'fs-extra';
+import { createGitlabToken, getGitlabToken } from './token';
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const { __setAnswers } = require('../utils');
 jest.mock('opn');
-jest.mock('fs-extra');
-jest.mock('../utils');
 jest.mock('../utils');
 
 beforeEach(() => {
@@ -18,15 +16,15 @@ beforeEach(() => {
 });
 
 test('create token (unkown git url)', async () => {
-  const dir = tempy.directory();
-  const token = await createGitlabToken(dir);
+  const filePath = tempy.file();
+  const token = await createGitlabToken(filePath);
 
   expect(token).toMatchInlineSnapshot(`"tH!S 1z th3 T0TA||Y S3CR3T T0Kn!"`);
 });
 
 test('create token (with git url)', async () => {
-  const dir = tempy.directory();
-  await createGitlabToken(dir, 'https://gitlab.example.com');
+  const filePath = tempy.file();
+  await createGitlabToken(filePath, 'https://gitlab.example.com');
 
   expect(opn).toHaveBeenLastCalledWith(
     expect.stringMatching(new RegExp('https://gitlab.example.com')),
@@ -35,8 +33,23 @@ test('create token (with git url)', async () => {
 });
 
 test('create token writes to file', async () => {
-  const dir = tempy.directory();
-  const token = await createGitlabToken(dir);
+  const filePath = tempy.file();
+  const token = await createGitlabToken(filePath);
 
-  expect(outputFile).toHaveBeenLastCalledWith(dir, token, expect.any(Object));
+  const contents = await readFile(filePath, 'utf8');
+  expect(token).toEqual(contents);
+  expect(contents).toMatchInlineSnapshot(`"tH!S 1z th3 T0TA||Y S3CR3T T0Kn!"`);
+});
+
+test('get Gitlab token (exists)', async () => {
+  const filePath = tempy.file();
+  await outputFile(filePath, 'GI$BZ"(NO"§');
+  const token = await getGitlabToken(filePath);
+  expect(token).toMatchInlineSnapshot(`"GI$BZ\\"(NO\\"§"`);
+});
+
+test('get Gitlab token (ENOENT)', async () => {
+  const filePath = tempy.file();
+  const token = await getGitlabToken(filePath);
+  expect(token).toMatchInlineSnapshot(`null`);
 });
